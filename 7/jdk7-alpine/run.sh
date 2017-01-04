010f286e981ef89028312e11eb3223873ea45fdb
denghui0115 <denghui0115@163.com>
2017-01-04 12:18:19 +0800

    Create run.sh

---
+++ 7/jdk7-alpine/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ ! -f /.tomcat_admin_created ]; then
+    create_tomcat_admin_user.sh
+fi
+
+export CATALINA_OPTS="$CATALINA_OPTS ${JAVA_OPTS}"
+
+exec ${CATALINA_HOME}/bin/catalina.sh run
